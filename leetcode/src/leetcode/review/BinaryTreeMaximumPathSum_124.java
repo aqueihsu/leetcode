1ff163fd1391eb605c32a348f27676850ecb3c16
aqueihsu <aquei.hsu@gmail.com>
2019-02-21 10:16:58 -0800

    miscs

--- leetcode/src/leetcode/review/BinaryTreeMaximumPathSum_124.java
+++ leetcode/src/leetcode/review/BinaryTreeMaximumPathSum_124.java
@@ -8,7 +8,26 @@ public class BinaryTreeMaximumPathSum_124 {
         TreeNode(int x) { val = x; }
     }
     
+    // [-2,null,-3], expected -2
+    int maxPathSum = Integer.MIN_VALUE;
+    
     public int maxPathSum(TreeNode root) {
+        maxDepthSum(root);
+        return maxPathSum;
+    }
+    
+    private int maxDepthSum(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        int leftSum = maxDepthSum(node.left);
+        int rightSum = maxDepthSum(node.right);
         
+        int leftMidSum = leftSum + node.val;
+        int rightMidSum = rightSum + node.val;
+        int midSum = leftSum + rightSum + node.val;
+        
+        maxPathSum = Math.max(Math.max(rightMidSum, rightMidSum), Math.max(midSum, maxPathSum));
+        return Math.max(leftSum, rightSum) + node.val;
     }
 }
